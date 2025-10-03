2c03a6a59405b6e6e404e99299dbc2f1f934b46f
leeseetheloswer <1443442148@qq.com>
2025-10-03 19:28:23 +0800

    init malluserservice

---
+++ MallUserService/src/main/java/com/leesee/malluserservice/MallUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.leesee.malluserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallUserServiceApplication.class, args);
+    }
+
+}
